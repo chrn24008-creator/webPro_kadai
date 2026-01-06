f48a3346b82a45837ba74f351124188e362c4a44
chrn24008-creator <chrn24008@g.nihon-u.ac.jp>
2026-01-06 18:31:56 +0900

    ファイルの追加

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.querySelector("#content")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
